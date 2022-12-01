3b970f1befc9286b487d9d7785410ffdab0d6cb0
Любовь Щербакова <Luba10.08@mail.ru>
2022-12-01 22:49:30 +0200

    popup choice language, correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
-let menuBurger = document.querySelector('.menuburger');
-let popup = document.querySelector('.popup');
+//Выплывающее меню в шапке
+const menuBurger = document.querySelector('.menuburger');
+const popup = document.querySelector('.popup');
 
 menuBurger.addEventListener('click', function () {
   popup.classList.toggle('popup-on');
+});
+
+const languageArrow = document.querySelector('.language__choice');
+//Выбор языка выплывающее окно
+const popupRu = document.querySelector('.language');
+const popupLanguage = document.querySelector('.language-popup');
+popupRu.addEventListener('click', function () {
+  popupLanguage.classList.toggle('language-popup-on');
+  languageArrow.classList.toggle('language__choice-on');
 });
